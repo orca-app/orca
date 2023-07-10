orca-app/orca

commit 9bfae2c4e8f87af77e27b5396b01ec6bd5333de5

[osx, metal] Set metal layer's device to layer.preferredDevice instead of calling MTLCreateDefaultSystemDevice(). This fixes issue 15 so we can use Discord's screen sharing while using Orca. This will need to be revisited when we want to actually use a different metal device.

src/mtl_surface.m
@@ -170,12 +170,18 @@ mg_surface_data* mg_mtl_surface_create_for_window(mp_window window)
 			//NOTE(martin): create a mtl device and a mtl layer and
 			//-----------------------------------------------------------
 
-			surface->device = MTLCreateSystemDefaultDevice();
-			[surface->device retain];
+			/*WARN(martin):
+				Calling MTLCreateDefaultSystemDevice(), as advised by the doc, hangs Discord's screen sharing...
+				The workaround I found, which doesn't make sense, is to set the mtlLayer.device to mtlLayer.preferredDevice,
+				even if mtlLayer.preferredDevice is the same value as returned by MTLCreateDefaultSystemDevice().
+				This works for now and allows screen sharing while using orca, but we'll have to revisit this when we want
+				more control over what GPU gets used.
+			*/
 			surface->mtlLayer = [CAMetalLayer layer];
 			[surface->mtlLayer retain];
+			surface->mtlLayer.device = surface->mtlLayer.preferredDevice;
+			surface->device = surface->mtlLayer.device;
 
-			surface->mtlLayer.device = surface->device;
 			[surface->mtlLayer setOpaque:NO];
 
 			[surface->interface.layer.caLayer addSublayer: (CALayer*)surface->mtlLayer];

src/graphics_surface.c
@@ -153,7 +153,7 @@ mg_surface mg_surface_create_for_window(mp_window window, mg_surface_api api)
 
 mg_surface mg_surface_create_remote(u32 width, u32 height, mg_surface_api api)
 {
-	if(__mgData.init)
+	if(!__mgData.init)
 	{
 		mg_init();
 	}
@@ -180,7 +180,7 @@ mg_surface mg_surface_create_remote(u32 width, u32 height, mg_surface_api api)
 
 mg_surface mg_surface_create_host(mp_window window)
 {
-	if(__mgData.init)
+	if(!__mgData.init)
 	{
 		mg_init();
 	}

commit a65b0cc1bb44ff79ca159ab4d495d93f4895c544

[osx, surface] Query list of available metal devices, and select a discrete GPU if available. This way we avoid calling MTLCreateSystemDefaultDevice() which messes with Discord's screen sharing, while also being able to take GPU frame captures in XCode (which was broken by 9bfae2c4e8)

src/mtl_surface.m
@@ -177,10 +177,30 @@ mg_surface_data* mg_mtl_surface_create_for_window(mp_window window)
 				This works for now and allows screen sharing while using orca, but we'll have to revisit this when we want
 				more control over what GPU gets used.
 			*/
+			surface->device = nil;
+
+			//Select a discrete GPU, if possible
+			NSArray<id<MTLDevice>>* devices = MTLCopyAllDevices();
+			for(id<MTLDevice> device in devices)
+			{
+				if(!device.isRemovable && !device.isLowPower)
+				{
+					surface->device = device;
+					break;
+				}
+			}
+			if(surface->device == nil)
+			{
+				log_warning("Couldn't select a discrete GPU, using first available device\n");
+				surface->device = devices[0];
+			}
+
+			//surface->device = MTLCreateSystemDefaultDevice();
+
 			surface->mtlLayer = [CAMetalLayer layer];
 			[surface->mtlLayer retain];
-			surface->mtlLayer.device = surface->mtlLayer.preferredDevice;
-			surface->device = surface->mtlLayer.device;
+			surface->mtlLayer.device = surface->device;
+
 
 			[surface->mtlLayer setOpaque:NO];
 