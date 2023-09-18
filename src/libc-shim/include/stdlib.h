34303edd4e028e8274c799f556041691cd04da54
Martin Fouilleul <martinfouilleul@gmail.com>
2023-09-18 20:12:44 +0200

    expose malloc/free to apps through libc-shim stdlib.h

--- src/libc-shim/include/stdlib.h
+++ src/libc-shim/include/stdlib.h
@@ -1,14 +1,20 @@
 #ifndef _STDLIB_H
 #define _STDLIB_H
 
+#include <stddef.h>
+
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 #define abort(...) OC_ABORT(__VA_ARGS__)
 
-    int abs(int);
+int abs(int);
+
+void* malloc(size_t);
+void* realloc(void*, size_t);
+void* calloc(size_t count, size_t size);
+void free(void*);
 
 #ifdef __cplusplus
 }
