25709a4935fe5d4a2659ced860984e5d20cefa23
Ilia Demianenko <ilia.demianenko@gmail.com>
2023-09-29 18:15:04 -0700

    UI updates
    UI sample: fix compile errors

--- doc/cheatsheets/cheatsheet_ui.h
+++ doc/cheatsheets/cheatsheet_ui.h
@@ -29,15 +29,17 @@ oc_ui_sig oc_ui_label(const char* label);
 oc_ui_sig oc_ui_label_str8(oc_str8 label);
 oc_ui_sig oc_ui_button(const char* label);
 oc_ui_sig oc_ui_checkbox(const char* name, bool* checked);
-oc_ui_box* oc_ui_slider(const char* label, f32 thumbRatio, f32* scrollValue);
+oc_ui_box* oc_ui_slider(const char* name, f32* value);
+oc_ui_box* oc_ui_scrollbar(const char* name, f32 thumbRatio, f32* scrollValue);
 oc_ui_text_box_result oc_ui_text_box(const char* name, oc_arena* arena, oc_str8 text);
 oc_ui_select_popup_info oc_ui_select_popup(const char* name, oc_ui_select_popup_info* info);
+oc_ui_radio_group_info oc_ui_radio_group(const char* name, oc_ui_radio_group_info* info);
 
 void oc_ui_panel_begin(const char* name, oc_ui_flags flags);
 void oc_ui_panel_end(void);
 #define oc_ui_panel(s, f)
 
-void oc_ui_menu_bar_begin(const char* label);
+void oc_ui_menu_bar_begin(const char* name);
 void oc_ui_menu_bar_end(void);
 #define oc_ui_menu_bar(name)
 
@@ -45,11 +47,11 @@ void oc_ui_menu_begin(const char* label);
 void oc_ui_menu_end(void);
 #define oc_ui_menu(name)
 
-oc_ui_sig oc_ui_menu_button(const char* name);
+oc_ui_sig oc_ui_menu_button(const char* label);
 
-oc_ui_sig oc_ui_tooltip_begin(const char* name);
+oc_ui_sig oc_ui_tooltip_begin(const char* label);
 void oc_ui_tooltip_end(void);
-#define oc_ui_tooltip(name)
+#define oc_ui_tooltip(label)
 
 //-------------------------------------------------------------------------------------
 // Styling
