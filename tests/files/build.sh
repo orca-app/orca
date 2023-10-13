b690b193b5a28e75630cfb0fddd83717838203cf
Martin Fouilleul <martinfouilleul@gmail.com>
2023-10-13 11:22:00 +0200

    update file io tests

--- tests/files/build.sh
+++ tests/files/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-LIBDIR=../../bin
-RESDIR=../../resources
-SRCDIR=../../milepost/src
+LIBDIR=../../build/bin
+SRCDIR=../../src
 
 INCLUDES="-I$SRCDIR -I$SRCDIR/util -I$SRCDIR/platform -I$SRCDIR/app"
-LIBS="-L$LIBDIR -lmilepost"
+LIBS="-L$LIBDIR -lorca"
 FLAGS="-mmacos-version-min=10.15.4 -DOC_DEBUG -DLOG_COMPILE_DEBUG"
 
 if [ ! \( -e bin \) ] ; then
@@ -14,4 +13,4 @@ fi
 
 
 clang -g $FLAGS $LIBS $INCLUDES -o ./bin/test_files main.c
-install_name_tool -add_rpath "@executable_path/../../../bin" ./bin/test_files
+install_name_tool -add_rpath "@executable_path/../../../build/bin" ./bin/test_files
