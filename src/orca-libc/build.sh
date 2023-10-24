7e730dc5ed171875efa917334b91c7a8d2b5ebf0
Martin Fouilleul <martinfouilleul@gmail.com>
2023-10-24 21:15:24 +0200

    add dummy crt to allow linking to orca-libc just by passing --sysroot=orca-libc

--- src/orca-libc/build.sh
+++ src/orca-libc/build.sh
@@ -2,13 +2,17 @@
 
 #cfiles=(src/{complex,ctype,errno,fenv,math,prng,search,stdio,stdlib,string}/*.c)
 
-cfiles=(src/{complex,ctype,errno,fenv,math,prng,string,stdlib,internal}/*.c)
+cfiles=(src/{complex,ctype,errno,fenv,math,prng,string,stdlib,stdio,internal}/*.c)
 
 warnings=(-Wall -Wextra -Werror -Wno-null-pointer-arithmetic -Wno-unused-parameter -Wno-sign-compare -Wno-unused-variable -Wno-unused-function -Wno-ignored-attributes -Wno-missing-braces -Wno-ignored-pragmas -Wno-unused-but-set-variable -Wno-unknown-warning-option -Wno-parentheses -Wno-shift-op-parentheses -Wno-bitwise-op-parentheses -Wno-logical-op-parentheses -Wno-string-plus-int -Wno-dangling-else -Wno-unknown-pragmas)
 
 includes=(-isystem ./include -Iarch -Isrc/internal)
 
-#/usr/local/opt/llvm/bin/clang -O2 -DNDEBUG --target=wasm32 --no-standard-libraries -fno-trapping-math -mbulk-memory -DBULK_MEMORY_THRESHOLD=32 -mthread-model single -MD -MP -Wl,--relocatable $warnings $includes -o stdlib.o $cfiles
+#/usr/local/opt/llvm/bin/clang -O2 -DNDEBUG --target=wasm32 --no-standard-libraries -fno-trapping-math -mbulk-memory -DBULK_MEMORY_THRESHOLD=32 -mthread-model single -MD -MP -Wl,--relocatable $warnings $includes -o lib/libc.o $cfiles
+
+#/usr/local/opt/llvm/bin/llvm-ar crs lib/libc.a lib/libc.o
+
+/usr/local/opt/llvm/bin/clang -O2 -DNDEBUG --target=wasm32 --no-standard-libraries -fno-trapping-math -mbulk-memory -DBULK_MEMORY_THRESHOLD=32 -mthread-model single -MD -MP -Wl,--relocatable $warnings $includes -o lib/crt1.o src/crt/crt1.c
 
 
-/usr/local/opt/llvm/bin/clang -O2 -DNDEBUG --target=wasm32 --no-standard-libraries -fno-trapping-math -mbulk-memory -DBULK_MEMORY_THRESHOLD=32 -mthread-model single -MD -MP -Wl,--no-entry $warnings $includes -o stdlib.o $cfiles
+/usr/local/opt/llvm/bin/clang -O2 --target=wasm32 --sysroot=. -Wl,--no-entry -o test test.c
---
+++ src/orca-libc/test.c
@@ -0,0 +1,7 @@
+#include<math.h>
+
+int main()
+{
+    double x = cos(3.14);
+    return(0);
+}
