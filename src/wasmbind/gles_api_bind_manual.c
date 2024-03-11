d70cecbb515387ea6c164bf876b46fd503228c03
Martin Fouilleul <martinfouilleul@gmail.com>
2024-03-11 18:17:41 +0100

    fix some static asserts without message, only valid since c23

--- src/wasmbind/gles_api_bind_manual.c
+++ src/wasmbind/gles_api_bind_manual.c
@@ -1119,7 +1119,7 @@ int manual_link_gles_api(oc_wasm* wasm)
     oc_wasm_status status;
     int ret = 0;
 
-    static_assert(OC_WASM_VALTYPE_I32 == 0);
+    static_assert(OC_WASM_VALTYPE_I32 == 0, "wasm i32 valtype should be 0");
     oc_wasm_valtype int_types[10] = { 0 };
 
     oc_wasm_binding binding = { 0 };
