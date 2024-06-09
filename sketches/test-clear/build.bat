9490fb5e5f54c58c0b874f75fddcd8349383a77c
martinfouilleul <martinfouilleul@gmail.com>
2024-06-09 16:06:23 +0200

    add test-clear sketch

---
+++ sketches/test-clear/build.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for /f %%i in ('orca sdk-path') do set ORCA_DIR=%%i
+
+:: common flags to build wasm modules
+set wasmFlags=--target=wasm32^
+       -mbulk-memory ^
+       -g -O2 ^
+       -Wl,--no-entry ^
+       -Wl,--export-dynamic ^
+       --sysroot %ORCA_DIR%/orca-libc ^
+       -I%ORCA_DIR%/src ^
+       -I%ORCA_DIR%/src/ext
+
+:: build sample as wasm module and link it with the orca module
+clang %wasmFlags% -L %ORCA_DIR%/bin -lorca_wasm -o module.wasm main.c
+IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+
+:: create app directory and copy files into it
+orca bundle --name Test module.wasm
--- src/runtime.c
+++ src/runtime.c
@@ -994,7 +994,7 @@ i32 orca_runloop(void* user)
         }
         else
         {
-            oc_set_color_rgba(0, 0, 0, 0);
+            oc_set_color_rgba(1, 0, 0, 1);
             oc_clear();
         }
 
