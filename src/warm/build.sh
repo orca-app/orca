1fe819a11b11751b9a8c961b6a572ef3ee0efb72
Martin Fouilleul <martinfouilleul@gmail.com>
2024-07-23 12:17:00 +0200

    basic json parser (for wasm tests)

--- src/warm/build.sh
+++ src/warm/build.sh
@@ -18,3 +18,8 @@ wasm2wat test/test.wasm > test/test.wat
 wat2wasm test/if.wat -o test/if.wasm
 wat2wasm test/block.wat -o test/block.wasm
 wat2wasm test/import.wat -o test/import.wasm
+
+
+# quick json test
+clang -g -I.. -I../ext -L../../build/bin -lorca -o bin/json_test json.c
+install_name_tool -add_rpath '@executable_path/' bin/json_test
