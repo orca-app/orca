e7c590e79fcdd998535ebad0b7549d3318942fce
Reuben Dunnington <reuben.dunnington@gmail.com>
2024-12-03 08:23:55 -0800

    wip - package-sdk command working

---
+++ src/build/package_sdk.zig
@@ -0,0 +1,217 @@
+const std = @import("std");
+
+fn trimWhitespace(str0: []const u8) []const u8 {
+    const trim = std.mem.trim;
+    const str1 = trim(u8, str0, "\n");
+    const str2 = trim(u8, str1, "\r");
+    const str3 = trim(u8, str2, " ");
+    return str3;
+}
+
+fn findGitVersion(arena: std.mem.Allocator) ![]const u8 {
+    const result: std.process.Child.RunResult = try std.process.Child.run(.{
+        .allocator = arena,
+        .argv = &.{ "git", "rev-parse", "--short", "HEAD" },
+        .cwd_dir = std.fs.cwd(),
+    });
+
+    switch (result.term) {
+        .Exited => |exit_code| {
+            if (exit_code != 0) {
+                std.log.err("git rev-parse failed with nonzero exit code {}", .{exit_code});
+                return error.GitRevParseFailed;
+            }
+        },
+        else => {
+            std.log.err("git rev-parse failed. stderr: {s}", .{result.stderr});
+            return error.GitRevParseFailed;
+        },
+    }
+
+    const output = trimWhitespace(result.stdout);
+    if (output.len == 0) {
+        std.log.err("git rev-parse output was empty", .{});
+        return error.GitRevParseUnexpectedOutput;
+    }
+
+    return output;
+}
+
+fn copyFolder(
+    allocator: std.mem.Allocator,
+    dest: []const u8,
+    src: []const u8,
+    include_extensions: []const []const u8,
+    ignore_patterns: []const []const u8,
+) !void {
+    std.log.info("copying '{s}' to '{s}'", .{ src, dest });
+
+    const cwd = std.fs.cwd();
+    try cwd.makePath(dest);
+
+    const src_dir: std.fs.Dir = try cwd.openDir(src, .{ .iterate = true });
+    const dest_dir: std.fs.Dir = try cwd.openDir(dest, .{ .iterate = true });
+
+    var normalized_ignore_patterns = std.ArrayList([]u8).init(allocator);
+    for (ignore_patterns) |pattern| {
+        try normalized_ignore_patterns.append(try std.fs.path.resolve(allocator, &.{pattern}));
+    }
+
+    var src_walker = try src_dir.walk(allocator);
+    while (try src_walker.next()) |src_entry| {
+        var included: bool = true;
+        if (src_entry.kind != .directory) {
+            included = include_extensions.len == 0;
+            const extension = std.fs.path.extension(src_entry.basename);
+            for (include_extensions) |included_extension| {
+                if (std.mem.eql(u8, extension, included_extension)) {
+                    included = true;
+                    break;
+                }
+            }
+        }
+
+        var ignored: bool = false;
+        for (normalized_ignore_patterns.items) |pattern| {
+            if (std.mem.indexOf(u8, src_entry.path, pattern) != null) {
+                ignored = true;
+                break;
+            }
+        }
+
+        if (included and !ignored) {
+            _ = switch (src_entry.kind) {
+                .file => try src_dir.updateFile(src_entry.path, dest_dir, src_entry.path, .{}),
+                else => {},
+            };
+        }
+    }
+}
+
+const Options = struct {
+    arena: std.mem.Allocator,
+    install_path: []const u8,
+    artifacts_path: []const u8,
+    resources_path: []const u8,
+    src_path: []const u8,
+    git_version: []const u8,
+
+    fn parse(args: []const [:0]const u8, arena: std.mem.Allocator) !Options {
+        var install_path: ?[]const u8 = null;
+        var artifacts_path: ?[]const u8 = null;
+        var resources_path: ?[]const u8 = null;
+        var src_path: ?[]const u8 = null;
+        var git_version: ?[]const u8 = null;
+        var is_dev_install: bool = false;
+
+        for (args, 0..) |raw_arg, i| {
+            if (i == 0) {
+                continue;
+            }
+
+            var splitIter = std.mem.splitScalar(u8, raw_arg, '=');
+            const arg: []const u8 = splitIter.next().?;
+            if (std.mem.eql(u8, arg, "--install-path")) {
+                install_path = splitIter.next();
+            } else if (std.mem.eql(u8, arg, "--artifacts-path")) {
+                artifacts_path = splitIter.next();
+            } else if (std.mem.eql(u8, arg, "--resources-path")) {
+                resources_path = splitIter.next();
+            } else if (std.mem.eql(u8, arg, "--src-path")) {
+                src_path = splitIter.next();
+            } else if (std.mem.eql(u8, arg, "--version")) {
+                git_version = splitIter.next();
+            } else if (std.mem.eql(u8, arg, "--dev")) {
+                is_dev_install = true;
+            }
+        }
+
+        var missing_arg: ?[]const u8 = null;
+        if (install_path == null) {
+            missing_arg = "install-path";
+        } else if (artifacts_path == null) {
+            missing_arg = "artifacts-path";
+        } else if (resources_path == null) {
+            missing_arg = "resources-path";
+        } else if (src_path == null) {
+            missing_arg = "src-path";
+        }
+
+        if (missing_arg) |arg| {
+            std.log.err("Missing required arg: {s}", .{arg});
+            return error.MissingRequiredArgument;
+        }
+
+        if (git_version == null) {
+            git_version = try findGitVersion(arena);
+        }
+
+        return Options{
+            .arena = arena,
+            .install_path = install_path.?,
+            .artifacts_path = artifacts_path.?,
+            .resources_path = resources_path.?,
+            .src_path = src_path.?,
+            .git_version = git_version.?,
+        };
+    }
+};
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator: std.mem.Allocator = arena.allocator();
+
+    const args: []const [:0]u8 = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    const opts = try Options.parse(args, allocator);
+
+    try std.fs.deleteTreeAbsolute(opts.install_path);
+
+    const src_paths: []const []const u8 = &.{
+        try std.fs.path.join(opts.arena, &.{ opts.artifacts_path, "bin" }),
+        try std.fs.path.join(opts.arena, &.{ opts.artifacts_path, "orca-libc" }),
+        opts.resources_path,
+    };
+
+    const dest_paths: []const []const u8 = &.{
+        try std.fs.path.join(opts.arena, &.{ opts.install_path, "bin" }),
+        try std.fs.path.join(opts.arena, &.{ opts.install_path, "orca-libc" }),
+        try std.fs.path.join(opts.arena, &.{ opts.install_path, "resources" }),
+    };
+
+    for (src_paths, dest_paths) |src, dest| {
+        try copyFolder(opts.arena, dest, src, &.{}, &.{});
+    }
+
+    const header_extensions: []const []const u8 = &.{
+        ".h",
+    };
+    // For copying the src directory, manually copy the curl and wasm3 dirs since we only care
+    // about the headers in a specific directory. Everything else is ok though
+    {
+        const dest_src_path = try std.fs.path.join(opts.arena, &.{ opts.install_path, "src" });
+        const ignore_patterns: []const []const u8 = &.{
+            "tool/",
+            "orca-libc/",
+            "wasm/",
+            "ext/curl/",
+            "ext/wasm3/",
+            "ext/zlib/build/", // copy all headers in zlib except zlib/build
+        };
+        try copyFolder(opts.arena, dest_src_path, opts.src_path, header_extensions, ignore_patterns);
+    }
+
+    {
+        const curl_src_path = try std.fs.path.join(opts.arena, &.{ opts.src_path, "ext/curl/include" });
+        const curl_dest_path = try std.fs.path.join(opts.arena, &.{ opts.install_path, "src/ext/curl/include" });
+        try copyFolder(opts.arena, curl_dest_path, curl_src_path, header_extensions, &.{});
+    }
+
+    {
+        const wasm3_src_path = try std.fs.path.join(opts.arena, &.{ opts.src_path, "ext/wasm3/source" });
+        const wasm3_dest_path = try std.fs.path.join(opts.arena, &.{ opts.install_path, "src/ext/wasm3/source" });
+        try copyFolder(opts.arena, wasm3_dest_path, wasm3_src_path, header_extensions, &.{});
+    }
+}
