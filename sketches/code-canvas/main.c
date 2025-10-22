e8876993c6583b7490f3906d01673de237043cdd
Martin Fouilleul <martinfouilleul@gmail.com>
2025-10-22 11:47:27 +0200

    [wip] card spacer algo helper: moving card along direction to avoid overlaps with marked cards

--- sketches/code-canvas/main.c
+++ sketches/code-canvas/main.c
@@ -77,6 +77,232 @@ void oc_code_canvas_create_card(oc_code_canvas* canvas, f32 x, f32 y)
 }
 
 /*
+void oc_card_spacer(oc_code_canvas* canvas, oc_card* movedCard, oc_vec2 delta)
+{
+    //TODO: create an initial state with all cards unmarked except the moved card
+
+    //TODO: iterate on the existing states to generate new states from them
+    {
+        //TODO: for each state
+        {
+            //TODO: if the intersection set is empty, put that state in the list of valid states and continue
+
+            //TODO: for each unmarked card that intersects with any card in the just moved set
+            {
+                //TODO: generate 4 axis aligned moves that pushed that card so that it doesn't intersect
+                // with any marked card
+                // prune moves that are opposite to the last move of the cards it intersects with (ie card must be pushed away, not swapped),
+                // unless we'd prune all moves
+                //TODO: for each created new states
+                {
+                    //TODO: copy that state N-1 times (N=number of moves), so that we get 4 states, to which we add
+                    // our for moves.
+                    // This
+                }
+            }
+            // we end up with M new states, some of which we must prune because the cards we just moved might overlap
+        }
+    }
+}
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//TODO: another outline where we don't have to prune after the fact, but keep things order-independent
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+typedef struct oc_card_spacer_item
+{
+    bool marked;
+    bool working;
+    oc_card* card;
+    oc_vec2 newPos;
+} oc_card_spacer_item;
+
+typedef struct oc_card_spacer_state
+{
+    oc_list_elt listElt;
+    oc_card_spacer_item* items;
+} oc_card_spacer_state;
+
+oc_card_spacer_state* oc_card_spacer_state_create(oc_arena* arena, u64 cardCount)
+{
+    oc_card_spacer_state* state = oc_arena_push_type(arena, oc_card_spacer_state);
+    state->items = oc_arena_push_array(arena, oc_card_spacer_item, cardCount);
+    return state;
+}
+
+typedef enum oc_card_spacer_direction
+{
+    OC_CARD_SPACER_UP,
+    OC_CARD_SPACER_DOWN,
+    OC_CARD_SPACER_LEFT,
+    OC_CARD_SPACER_RIGHT,
+} oc_card_spacer_direction;
+
+oc_vec2 oc_card_spacer_move(oc_card_spacer_state* state, u64 itemIndex, oc_card_spacer_direction direction, u64 cardCount)
+{
+    //NOTE: pushes a card in one direction so that it doesn't intersect any marked cards
+
+    oc_vec2 newPos = state->items[itemIndex].card->rect.xy;
+
+    u64 intersectCount = 1;
+    while(intersectCount)
+    {
+        intersectCount = 0;
+        for(u64 i = 0; i < cardCount; i++)
+        {
+            if(state->items[i].marked
+               && oc_card_spacer_intersect(&state->items[i], &state->items[itemIndex]))
+            {
+                intersectCount++;
+
+                oc_rect itemRect = {
+                    .xy = newPos,
+                    .wh = state->items[itemIndex].card->rect.wh,
+                };
+                oc_rect intersectedRect = {
+                    .xy = state->items[i].newPos,
+                    .wh = state->items[i].card->rect.wh,
+                };
+
+                switch(direction)
+                {
+                    case OC_CARD_SPACER_UP:
+                    {
+                        newPos.y = intersectedRect.y - state->items[itemIndex].card->rect.h;
+                    }
+                    break;
+                    case OC_CARD_SPACER_DOWN:
+                    {
+                        newPos.y = intersectedRect.y + intersectedRect.h;
+                    }
+                    break;
+                    case OC_CARD_SPACER_LEFT:
+                    {
+                        newPos.x = intersectedRect.x - state->items[itemIndex].card->rect.w;
+                    }
+                    break;
+                    case OC_CARD_SPACER_RIGHT:
+                    {
+                        newPos.x = intersectedRect.x + intersectedRect.w;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    return newPos;
+}
+
+void oc_card_spacer(oc_code_canvas* canvas, oc_card* movedCard, oc_vec2 delta)
+{
+    // a "state" consists of
+    // - unmarked cards: cards that have not been moved yet
+    // - a working set: cards that have been moved and not examined for new collisions
+    // - marked cards: cards that have been moved and have pushed any other intersecting cards
+    // a state with an empty working set is a valid state
+
+    oc_arena_scope scratch = oc_scratch_begin();
+    oc_list states = { 0 };
+
+    // Create an initial state with all cards unmarked except the moved card, which is marked and
+    // in the working set
+
+    oc_card_spacer_state* initialState = oc_card_spacer_state_create(scratch.arena, canvas->cardCount);
+    oc_list_for_indexed(canvas->cards, it, oc_card, listElt)
+    {
+        initialState->items[it.index].card = it.elt;
+        if(it.elt == movedCard)
+        {
+            initialState->items[it.index].marked = true;
+            initialState->items[it.index].working = true;
+        }
+    }
+    oc_list_push_back(&states, &initialState->listElt);
+
+    oc_list validStates = { 0 };
+    u32 validStateCount = 0;
+
+    //iterate on the existing states to generate new states, until we have enough valid states
+    while(validStateCount < 4)
+    {
+        oc_list newStates = { 0 };
+        oc_list_for_safe(states, state, oc_card_spacer_state, listElt)
+        {
+            // for each card Cw in the working set
+            for(u64 workingIndex = 0; workingIndex < canvas->cardCount; workingIndex++)
+            {
+                if(state->items[workingIndex].working)
+                {
+                    // for each unmarked card Cj that intersect with Cw
+                    u64 intersectCount = 0;
+                    for(u64 intersectIndex = 0; intersectIndex < canvas->cardCount; intersectIndex++)
+                    {
+                        if(!state->items[intersectIndex].marked
+                           && oc_card_spacer_intersect(&state->items[workingIndex], &state->items[intersectIndex]))
+                        {
+                            intersectCount++;
+                            // generate 4 axis aligned moves that push that card so that Cj doesn't intersect with any marked card.
+                            // prune moves that are opposite to the moves of the cards it intersects with in the working set
+                            // (note: this avoids cards being swapped)
+
+                            u32 moveCount = 0;
+                            oc_vec2 moves[4];
+                            for(u32 dirIndex = 0; dirIndex < 4; dirIndex++)
+                            {
+                                bool prune = false;
+                                for(u64 pusherIndex = 0; pusherIndex < canvas->cardCount; pusherIndex++)
+                                {
+                                    if(state->items[pusherIndex].working
+                                       && oc_card_spacer_intersect(&state->items[pusherIndex], &state->items[intersectIndex]))
+                                    {
+                                        //TODO: if direction of pusher is opposite of dirindex, prune
+                                    }
+                                }
+                                if(!prune)
+                                {
+                                    //TODO generate move in direction dirIndex
+                                }
+                            }
+
+                            // for each move, create a new state by appending to state
+                            for(u32 moveIndex = 0; moveIndex < moveCount; moveIndex++)
+                            {
+                                oc_card_spacer_state* newState = oc_card_spacer_state_create(scratch.arena, canvas->cardCount);
+                                memcpy(newState->items, state->items, canvas->cardCount * sizeof(oc_card_spacer_item));
+
+                                newState->items[workingIndex].working = false;
+                                newState->items[intersectIndex].marked = true;
+                                newState->items[intersectIndex].working = true;
+                                newState->items[intersectIndex].newPos = oc_vec2_add(newState->items[intersectIndex].card->rect.xy,
+                                                                                     moves[moveIndex]);
+
+                                oc_list_push_back(&newStates, &newState->listElt);
+                            }
+                        }
+                    }
+                    if(!intersectCount)
+                    {
+                        oc_list_remove(&states, &state->listElt);
+                        oc_list_push_back(&validStates, &state->listElt);
+                        validStateCount++;
+                    }
+                }
+            }
+        }
+    }
+
+    //TODO: select best valid state
+    oc_card_spacer_state* bestState = oc_list_first_entry(validStates, oc_card_spacer_state, listElt);
+    for(u64 i = 0; i < canvas->cardCount; i++)
+    {
+        bestState->items[i].card->rect.xy = bestState->items[i].newPos;
+    }
+
+    oc_scratch_end(scratch);
+}
+*/
+
+/*
 typedef struct oc_card_spacer_move
 {
     oc_card* card;
@@ -152,9 +378,136 @@ void oc_card_spacer(oc_code_canvas* canvas, oc_card* movedCard, oc_vec2 delta)
 enum
 {
     OC_CARD_BORDER_SIZE = 4,
+    OC_CARD_SPACER_MARGIN = 16,
     OC_CARD_MIN_WIDTH = 50,
     OC_CARD_MIN_HEIGHT = 50,
 };
+
+typedef enum oc_card_spacer_direction
+{
+    OC_CARD_SPACER_UP,
+    OC_CARD_SPACER_DOWN,
+    OC_CARD_SPACER_LEFT,
+    OC_CARD_SPACER_RIGTH,
+} oc_card_spacer_direction;
+
+typedef struct oc_card_spacer_item
+{
+    oc_card* card;
+
+    oc_vec2 newPos;
+    bool marked;
+    bool working;
+
+} oc_card_spacer_item;
+
+typedef struct oc_card_spacer_state
+{
+    u64 itemCount;
+    oc_card_spacer_item* items;
+
+} oc_card_spacer_state;
+
+bool oc_rect_overlap(oc_rect a, oc_rect b)
+{
+    bool overlap = (a.x < b.x + b.w)
+                && (a.x + a.w > b.x)
+                && (a.y < b.y + b.h)
+                && (a.y + a.h > b.y);
+    return overlap;
+}
+
+oc_vec2 oc_card_spacer_move_direction(oc_card_spacer_state* state, oc_card_spacer_item* item, oc_card_spacer_direction direction)
+{
+    bool intersect = false;
+    oc_vec2 newPos = item->card->rect.xy;
+
+    do
+    {
+        intersect = false;
+        for(u64 i = 0; i < state->itemCount; i++)
+        {
+            oc_card_spacer_item* other = &state->items[i];
+            if(other->marked)
+            {
+                oc_rect rectItem = {
+                    .xy = newPos,
+                    .wh = item->card->rect.wh,
+                };
+
+                oc_rect rectOther = {
+                    other->newPos.x - OC_CARD_SPACER_MARGIN,
+                    other->newPos.y - OC_CARD_SPACER_MARGIN,
+                    other->card->rect.w + 2 * OC_CARD_SPACER_MARGIN,
+                    other->card->rect.h + 2 * OC_CARD_SPACER_MARGIN,
+                };
+
+                if(oc_rect_overlap(rectItem, rectOther))
+                {
+                    intersect = true;
+                    switch(direction)
+                    {
+                        case OC_CARD_SPACER_UP:
+                        {
+                            newPos.y = other->newPos.y - item->card->rect.h - OC_CARD_SPACER_MARGIN;
+                        }
+                        break;
+                        case OC_CARD_SPACER_DOWN:
+                        {
+                            newPos.y = other->newPos.y + other->card->rect.h + OC_CARD_SPACER_MARGIN;
+                        }
+                        break;
+                        case OC_CARD_SPACER_LEFT:
+                        {
+                            newPos.x = other->newPos.x - item->card->rect.w - OC_CARD_SPACER_MARGIN;
+                        }
+                        break;
+                        case OC_CARD_SPACER_RIGTH:
+                        {
+                            newPos.x = other->newPos.x + other->card->rect.w + OC_CARD_SPACER_MARGIN;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    while(intersect);
+
+    return newPos;
+}
+
+void oc_card_spacer(oc_code_canvas* canvas, oc_card* initialCard)
+{
+    oc_arena_scope scratch = oc_scratch_begin();
+
+    oc_card_spacer_state state = {
+        .itemCount = canvas->cardCount,
+        .items = oc_arena_push_array(scratch.arena, oc_card_spacer_item, canvas->cardCount),
+    };
+
+    oc_card_spacer_item* item = 0;
+    oc_list_for_indexed(canvas->cards, it, oc_card, listElt)
+    {
+        state.items[it.index].card = it.elt;
+        state.items[it.index].newPos = it.elt->rect.xy;
+
+        //TODO: test, change that
+        if(it.elt != initialCard)
+        {
+            state.items[it.index].marked = true;
+        }
+        else
+        {
+            item = &state.items[it.index];
+        }
+    }
+
+    //TODO test, change that
+    item->card->rect.xy = oc_card_spacer_move_direction(&state, item, OC_CARD_SPACER_UP);
+
+    oc_scratch_end(scratch);
+}
 
 void oc_card_ui(oc_code_canvas* canvas, oc_card* card)
 {
@@ -310,6 +663,11 @@ i32 ui_runloop(void* user)
         {
             if(oc_mouse_released(oc_ui_input(), OC_MOUSE_LEFT))
             {
+                if(canvas.draggedCard)
+                {
+                    oc_card_spacer(&canvas, canvas.draggedCard);
+                }
+
                 canvas.draggedCard = 0;
                 canvas.resizedCard = 0;
                 canvas.resizedBorders = 0;
