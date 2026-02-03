042943c0ebf0f356a4b32bbd59885f669e819b14
Martin Fouilleul <martinfouilleul@gmail.com>
2026-02-03 22:26:54 +0100

    continue working on new wasm bindings generator

--- src/build/gen_host_interface.c
+++ src/build/gen_host_interface.c
@@ -13,6 +13,7 @@ json_node* json_expect_field(json_node* parent, oc_str8 name, json_node_kind kin
     if(!child)
     {
         printf("Error: field %.*s doesn't exist\n", oc_str8_ip(name));
+        exit(-1);
     }
     else if(child->kind != kind)
     {
@@ -20,39 +21,25 @@ json_node* json_expect_field(json_node* parent, oc_str8 name, json_node_kind kin
                json_node_kind_strings[child->kind],
                oc_str8_ip(name),
                json_node_kind_strings[kind]);
-        child = 0;
+        exit(-1);
     }
     return child;
 }
 
-int gen_type(oc_arena* arena, oc_str8_list* list, json_node* node)
+void gen_type(oc_arena* arena, oc_str8_list* list, json_node* node)
 {
-    int result = 0;
-
     json_node* kindNode = json_expect_field(node, OC_STR8("kind"), JSON_STRING);
-    if(!kindNode)
-    {
-        result = -1;
-        goto end;
-    }
+
     if(!oc_str8_cmp(kindNode->string, OC_STR8("namedType")))
     {
         json_node* nameNode = json_expect_field(node, OC_STR8("name"), JSON_STRING);
-        if(!nameNode)
-        {
-            result = -1;
-            goto end;
-        }
+
         oc_str8_list_push(arena, list, nameNode->string);
     }
     else if(!oc_str8_cmp(kindNode->string, OC_STR8("pointer")))
     {
         json_node* typeNode = json_expect_field(node, OC_STR8("type"), JSON_OBJECT);
-        if(!typeNode)
-        {
-            result = -1;
-            goto end;
-        }
+
         gen_type(arena, list, typeNode);
         oc_str8_list_push(arena, list, OC_STR8("*"));
     }
@@ -60,80 +47,22 @@ int gen_type(oc_arena* arena, oc_str8_list* list, json_node* node)
     {
         oc_str8_list_push(arena, list, kindNode->string);
     }
-
-end:
-    return result;
 }
 
-int pass_by_value(json_node* root, json_node* typeNode, bool* passByValue)
+void gen_hostcall_prototype(oc_arena* arena, oc_str8_list* list, json_node* root, json_node* procNode)
 {
-    int result = 0;
-
-    json_node* typeKindNode = json_expect_field(typeNode, OC_STR8("kind"), JSON_STRING);
-    if(!typeKindNode)
-    {
-        result = -1;
-        goto end;
-    }
-
-    if(!oc_str8_cmp(typeKindNode->string, OC_STR8("struct")))
-    {
-        *passByValue = false;
-    }
-    else
-    {
-        *passByValue = true;
-    }
-
-end:
-    return result;
-}
-
-int gen_hostcall_prototype(oc_arena* arena, oc_str8_list* list, json_node* root, json_node* procNode)
-{
-    int result = 0;
-
     json_node* nameNode = json_expect_field(procNode, OC_STR8("name"), JSON_STRING);
-    if(!nameNode)
-    {
-        result = -1;
-        goto end;
-    }
     json_node* returnNode = json_expect_field(procNode, OC_STR8("return"), JSON_OBJECT);
-    if(!returnNode)
-    {
-        result = -1;
-        goto end;
-    }
     json_node* paramsNode = json_expect_field(procNode, OC_STR8("params"), JSON_LIST);
-    if(!paramsNode)
-    {
-        result = -1;
-        goto end;
-    }
 
-    bool returnByValue;
-    if(pass_by_value(root, returnNode, &returnByValue))
-    {
-        result = -1;
-        goto end;
-    }
-
-    if(returnByValue)
-    {
-        gen_type(arena, list, returnNode);
-    }
-    else
-    {
-        oc_str8_list_push(arena, list, OC_STR8("void"));
-    }
+    gen_type(arena, list, returnNode);
 
     oc_str8_list_pushf(arena,
                        list,
                        " %.*s(",
                        oc_str8_ip(nameNode->string));
 
-    if(oc_list_empty(paramsNode->children) && returnByValue)
+    if(oc_list_empty(paramsNode->children))
     {
         oc_str8_list_push(arena, list, OC_STR8("void"));
     }
@@ -141,39 +70,21 @@ int gen_hostcall_prototype(oc_arena* arena, oc_str8_list* list, json_node* root,
     {
         if(paramNode->kind != JSON_OBJECT)
         {
-            result = -1;
-            goto end;
+            printf("Error: param specification must be an object.\n");
+            exit(-1);
         }
         json_node* paramNameNode = json_expect_field(paramNode, OC_STR8("name"), JSON_STRING);
-        if(!paramNameNode)
-        {
-            result = -1;
-            goto end;
-        }
         json_node* paramTypeNode = json_expect_field(paramNode, OC_STR8("type"), JSON_OBJECT);
-        if(!paramTypeNode)
-        {
-            result = -1;
-            goto end;
-        }
+
         gen_type(arena, list, paramTypeNode);
         oc_str8_list_pushf(arena, list, " %.*s", oc_str8_ip(paramNameNode->string));
-        if(paramNode->listElt.next || !returnByValue)
+        if(paramNode->listElt.next)
         {
             oc_str8_list_push(arena, list, OC_STR8(", "));
         }
     }
 
-    if(!returnByValue)
-    {
-        gen_type(arena, list, returnNode);
-        oc_str8_list_push(arena, list, OC_STR8("* _returnPointer"));
-    }
-
     oc_str8_list_push(arena, list, OC_STR8(");\n"));
-
-end:
-    return result;
 }
 
 json_node* find_named_type(json_node* root, oc_str8 name)
@@ -181,17 +92,11 @@ json_node* find_named_type(json_node* root, oc_str8 name)
     oc_list_for(root->children, child, json_node, listElt)
     {
         json_node* kindNode = json_expect_field(child, OC_STR8("kind"), JSON_STRING);
-        if(!kindNode)
-        {
-            return 0;
-        }
+
         if(!oc_str8_cmp(kindNode->string, OC_STR8("typename")))
         {
             json_node* nameNode = json_expect_field(child, OC_STR8("name"), JSON_STRING);
-            if(!nameNode)
-            {
-                return 0;
-            }
+
             if(!oc_str8_cmp(nameNode->string, name))
             {
                 return child;
@@ -200,30 +105,19 @@ json_node* find_named_type(json_node* root, oc_str8 name)
     }
 
     printf("Error: typename %.*s not found\n", oc_str8_ip(name));
+    exit(-1);
     return 0;
 }
 
-int get_native_type(oc_arena* arena, oc_str8* typeString, json_node* root, json_node* node)
+oc_str8 get_native_type(oc_arena* arena, json_node* root, json_node* node)
 {
+    oc_str8 typeString = { 0 };
     json_node* kindNode = json_expect_field(node, OC_STR8("kind"), JSON_STRING);
-    if(!kindNode)
-    {
-        return -1;
-    }
 
     if(!oc_str8_cmp(kindNode->string, OC_STR8("namedType")))
     {
         json_node* nameNode = json_expect_field(node, OC_STR8("name"), JSON_STRING);
-        if(!nameNode)
-        {
-            return -1;
-        }
-
         json_node* typenameNode = find_named_type(root, nameNode->string);
-        if(!typenameNode)
-        {
-            return -1;
-        }
 
         json_node* hostNode = json_find(typenameNode, OC_STR8("host"));
         if(hostNode)
@@ -231,134 +125,183 @@ int get_native_type(oc_arena* arena, oc_str8* typeString, json_node* root, json_
             if(hostNode->kind != JSON_STRING)
             {
                 printf("Error: 'host' property should be a string.\n");
-                return -1;
+                exit(-1);
             }
-            *typeString = hostNode->string;
+            typeString = hostNode->string;
         }
         else
         {
-            *typeString = nameNode->string;
+            typeString = nameNode->string;
         }
     }
     else if(!oc_str8_cmp(kindNode->string, OC_STR8("pointer")))
     {
         json_node* typeNode = json_expect_field(node, OC_STR8("type"), JSON_OBJECT);
-        if(!typeNode)
-        {
-            return -1;
-        }
-        oc_str8 sub = { 0 };
-        if(get_native_type(arena, &sub, root, typeNode))
-        {
-            return -1;
-        }
-        *typeString = oc_str8_pushf(arena, "%.*s*", oc_str8_ip(sub));
+
+        oc_str8 sub = get_native_type(arena, root, typeNode);
+
+        typeString = oc_str8_pushf(arena, "%.*s*", oc_str8_ip(sub));
     }
     else if(!oc_str8_cmp(kindNode->string, OC_STR8("struct")))
     {
         printf("Error: struct type found where namedType would be expected\n");
-        return -1;
+        exit(-1);
     }
     else
     {
-        *typeString = kindNode->string;
+        typeString = kindNode->string;
     }
-
-    return 0;
+    return typeString;
 }
 
-int gen_hostapi_binding(oc_arena* arena, oc_str8_list* list, json_node* root, json_node* procNode)
+json_node* unwrap_named_types(json_node* root, json_node* node)
 {
-    int result = 0;
+    json_node* kindNode = json_expect_field(node, OC_STR8("kind"), JSON_STRING);
 
+    if(!oc_str8_cmp(kindNode->string, OC_STR8("namedType")))
+    {
+        json_node* nameNode = json_expect_field(node, OC_STR8("name"), JSON_STRING);
+        json_node* typenameNode = find_named_type(root, nameNode->string);
+        json_node* typeNode = json_expect_field(typenameNode, OC_STR8("type"), JSON_OBJECT);
+
+        return unwrap_named_types(root, typeNode);
+    }
+    else
+    {
+        return node;
+    }
+}
+
+typedef struct param_type_desc
+{
+    oc_str8 string;
+    bool isPointer;
+} param_type_desc;
+
+typedef struct param_desc
+{
+    oc_list_elt listElt;
+    oc_str8 name;
+    param_type_desc typeDesc;
+
+} param_desc;
+
+param_type_desc parse_param_type(oc_arena* arena, json_node* root, json_node* node)
+{
+    param_type_desc desc = { 0 };
+
+    json_node* unwrappedTypeNode = unwrap_named_types(root, node);
+    json_node* unwrappedKindNode = json_expect_field(unwrappedTypeNode, OC_STR8("kind"), JSON_STRING);
+
+    if(!oc_str8_cmp(unwrappedKindNode->string, OC_STR8("pointer")))
+    {
+        desc.isPointer = true;
+    }
+    else if(!oc_str8_cmp(unwrappedKindNode->string, OC_STR8("struct")))
+    {
+        printf("Error: hostcall param should be a scalar or pointer.\n");
+        exit(-1);
+    }
+    desc.string = get_native_type(arena, root, node);
+    return desc;
+}
+
+param_desc* parse_param(oc_arena* arena, oc_list* paramList, json_node* root, json_node* paramNode)
+{
+    json_node* paramNameNode = json_expect_field(paramNode, OC_STR8("name"), JSON_STRING);
+    json_node* paramTypeNode = json_expect_field(paramNode, OC_STR8("type"), JSON_OBJECT);
+
+    json_node* unwrappedTypeNode = unwrap_named_types(root, paramTypeNode);
+    json_node* unwrappedKindNode = json_expect_field(unwrappedTypeNode, OC_STR8("kind"), JSON_STRING);
+
+    param_desc* param = oc_arena_push_type(arena, param_desc);
+    param->name = paramNameNode->string;
+    param->typeDesc = parse_param_type(arena, root, paramTypeNode);
+
+    oc_list_push_back(paramList, &param->listElt);
+
+    return param;
+}
+
+void gen_hostapi_binding(oc_arena* arena, oc_str8_list* list, json_node* root, json_node* procNode)
+{
     json_node* nameNode = json_expect_field(procNode, OC_STR8("name"), JSON_STRING);
-    if(!nameNode)
-    {
-        result = -1;
-        goto end;
-    }
     json_node* handlerNode = json_expect_field(procNode, OC_STR8("handler"), JSON_STRING);
-    if(!handlerNode)
-    {
-        result = -1;
-        goto end;
-    }
     json_node* returnNode = json_expect_field(procNode, OC_STR8("return"), JSON_OBJECT);
-    if(!returnNode)
-    {
-        result = -1;
-        goto end;
-    }
     json_node* paramsNode = json_expect_field(procNode, OC_STR8("params"), JSON_LIST);
-    if(!paramsNode)
-    {
-        result = -1;
-        goto end;
-    }
 
     oc_str8_list_pushf(arena,
                        list,
                        "void %.*s_stub(wa_interpreter* interpreter, wa_value* params, wa_value* returns, void* user)\n{\n",
                        oc_str8_ip(handlerNode->string));
 
+    param_type_desc returnDesc = parse_param_type(arena, root, returnNode);
+
+    oc_list params = { 0 };
+
     oc_list_for_indexed(paramsNode->children, it, json_node, listElt)
     {
         json_node* paramNode = it.elt;
 
-        json_node* paramNameNode = json_expect_field(paramNode, OC_STR8("name"), JSON_STRING);
-        if(!paramNameNode)
-        {
-            result = -1;
-            goto end;
-        }
-        json_node* paramTypeNode = json_expect_field(paramNode, OC_STR8("type"), JSON_OBJECT);
-        if(!paramTypeNode)
-        {
-            result = -1;
-            goto end;
-        }
+        param_desc* param = parse_param(arena, &params, root, paramNode);
 
-        oc_str8 paramType = { 0 };
-        if(get_native_type(arena, &paramType, root, paramTypeNode))
-        {
-            result = -1;
-            goto end;
-        }
-
-        bool passByValue;
-        if(pass_by_value(root, paramType, &passByValue))
-        {
-            return -1;
-        }
-        if(passByValue)
+        if(param->typeDesc.isPointer)
         {
             oc_str8_list_pushf(arena,
                                list,
-                               "\t%.*s %.*s = *(%.*s*)&params[%llu];\n",
-                               oc_str8_ip(paramType),
-                               oc_str8_ip(paramNameNode->string),
-                               oc_str8_ip(paramType),
+                               "\t%.*s %.*s = (%.*s)((char*)mem + *(i32*)&params[%llu]);\n",
+                               oc_str8_ip(param->typeDesc.string),
+                               oc_str8_ip(param->name),
+                               oc_str8_ip(param->typeDesc.string),
                                it.index);
+
+            oc_str8_list_pushf(arena,
+                               list,
+                               "\tOC_ASSERT_DIALOG(((char*)%.*s >= (char*)_mem) && ((char*)%.*s - (char*)_mem) < _memSize, \"pointer is out of bounds\");\n",
+                               oc_str8_ip(param->name),
+                               oc_str8_ip(param->name));
+
+            //TODO: apply pointer validation rules
         }
         else
         {
             oc_str8_list_pushf(arena,
                                list,
-                               "\t%.*s* %.*s = (%.*s*)((char*)mem + *(i32*)&params[%llu]);\n",
-                               oc_str8_ip(paramType),
-                               oc_str8_ip(paramNameNode->string),
-                               oc_str8_ip(paramType),
+                               "\t%.*s %.*s = *(%.*s*)&params[%llu];\n",
+                               oc_str8_ip(param->typeDesc.string),
+                               oc_str8_ip(param->name),
+                               oc_str8_ip(param->typeDesc.string),
                                it.index);
-
-            //TODO: check that pointer is valid
         }
     }
-    //TODO: call handler
 
-    oc_str8_list_push(arena, list, OC_STR8("}\n"));
+    if(!oc_list_empty(params))
+    {
+        oc_str8_list_push(arena, list, OC_STR8("\n"));
+    }
 
-end:
-    return result;
+    oc_str8_list_push(arena, list, OC_STR8("\t"));
+
+    if(oc_str8_cmp(returnDesc.string, OC_STR8("void")))
+    {
+        if(returnDesc.isPointer)
+        {
+            printf("Error: return type should be a scalar.\n");
+            exit(-1);
+        }
+        oc_str8_list_pushf(arena, list, "*(%.*s*)&ret[0] = ", oc_str8_ip(returnDesc.string));
+    }
+
+    oc_str8_list_pushf(arena, list, "%.*s(", oc_str8_ip(handlerNode->string));
+    oc_list_for(params, param, param_desc, listElt)
+    {
+        oc_str8_list_push(arena, list, param->name);
+        if(param->listElt.next)
+        {
+            oc_str8_list_push(arena, list, OC_STR8(", "));
+        }
+    }
+    oc_str8_list_push(arena, list, OC_STR8(");\n}\n\n"));
 }
 
 int main(int argc, char** argv)
@@ -440,32 +383,13 @@ int main(int argc, char** argv)
             if(child->kind == JSON_OBJECT)
             {
                 json_node* kindNode = json_expect_field(child, OC_STR8("kind"), JSON_STRING);
-                if(!kindNode)
-                {
-                    result = -1;
-                    goto end;
-                }
 
                 if(!oc_str8_cmp(kindNode->string, OC_STR8("typename")))
                 {
                     json_node* nameNode = json_expect_field(child, OC_STR8("name"), JSON_STRING);
-                    if(!nameNode)
-                    {
-                        result = -1;
-                        goto end;
-                    }
                     json_node* typeNode = json_expect_field(child, OC_STR8("type"), JSON_OBJECT);
-                    if(!typeNode)
-                    {
-                        result = -1;
-                        goto end;
-                    }
                     json_node* typeKindNode = json_expect_field(typeNode, OC_STR8("kind"), JSON_STRING);
-                    if(!typeKindNode)
-                    {
-                        result = -1;
-                        goto end;
-                    }
+
                     if(!oc_str8_cmp(typeKindNode->string, OC_STR8("struct")))
                     {
                         oc_str8_list_pushf(scratch.arena,
@@ -485,29 +409,18 @@ int main(int argc, char** argv)
             if(child->kind == JSON_OBJECT)
             {
                 json_node* kindNode = json_expect_field(child, OC_STR8("kind"), JSON_STRING);
-                if(!kindNode)
-                {
-                    result = -1;
-                    goto end;
-                }
 
                 if(!oc_str8_cmp(kindNode->string, OC_STR8("proc")))
                 {
                     //NOTE: generate hostcall definition
-                    if(gen_hostcall_prototype(scratch.arena, &hostcallDeclList, root, child))
-                    {
-                        result = -1;
-                        goto end;
-                    }
-
-                    if(gen_hostapi_binding(scratch.arena, &hostapiBindingList, root, child))
-                    {
-                        result = -1;
-                        goto end;
-                    }
+                    gen_hostcall_prototype(scratch.arena, &hostcallDeclList, root, child);
+                    gen_hostapi_binding(scratch.arena, &hostapiBindingList, root, child);
                 }
             }
         }
+
+        //TODO: generate code to bind stubs to wasm import names
+
         printf("\n --- host call declarations ---\n");
         oc_str8 hostcallDecl = oc_str8_list_join(scratch.arena, hostcallDeclList);
         printf("%.*s\n", oc_str8_ip(hostcallDecl));
